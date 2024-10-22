dfcabb5edac68ab831220c335bd485bddd85b8ba
blurk <nsinh6745@gmail.com>
2024-10-22 22:46:12 +0700

    04

--- chap-01/main.js
+++ chap-01/main.js
@@ -11,7 +11,38 @@ const material = new THREE.MeshBasicMaterial(
   { color: "red", wireframe: false }
 );
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+mesh.position.set(0.7, -0.6, 1);
+mesh.scale.set(2, 0.5, 0.5);
+// Change rotation order
+mesh.rotation.reorder("YXZ");
+mesh.rotation.x = Math.PI * 0.25;
+mesh.rotation.y = Math.PI * 0.25;
+// scene.add(mesh);
+
+const axisHelper = new THREE.AxesHelper(3);
+scene.add(axisHelper);
+
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = 2;
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = -2;
+
+group.add(cube1, cube2, cube3);
 
 const sizes = {
   width: 800,
@@ -19,13 +50,17 @@ const sizes = {
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.set(0, 1, 3);
+camera.position.z = 7;
 scene.add(camera);
+
+// camera.lookAt(mesh.position);
 
 const renderer = new THREE.WebGLRenderer(
   /** @type {import("three").WebGLRendererParameters} */ { canvas }
 );
 
 renderer.setSize(sizes.width, sizes.height);
-
+group.position.y = 1;
+group.scale.x = 2;
+group.rotation.z = Math.PI * 0.25;
 renderer.render(scene, camera);
